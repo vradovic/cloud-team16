7017bac0d3e718a105e7156a2bca66d84ae8ada4
Vladislav Radovic <vradovic522@gmail.com>
2024-06-22 23:21:20 +0200

    Remove unecessary imports

--- app/aws-exports.js
+++
@@ -1,9 +0,0 @@
-const awsmobile = {
-  "aws_project_region": "eu-central-1",
-  "aws_cognito_region": "eu-central-1",
-  "aws_user_pools_id": "eu-central-1_A16JOJsMT",
-  "aws_user_pools_web_client_id": "4ius91dsuomduu3bjlqcshndqf",
-  "aws_bucket_name": "srbflixbucket"
-};
-
-export default awsmobile;
