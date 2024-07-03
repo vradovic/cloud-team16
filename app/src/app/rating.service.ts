806fa06ff0c9ae5d90d104414446223c30364ad3
Vladislav Radovic <vradovic522@gmail.com>
2024-07-03 21:25:51 +0200

    Setup prod env

--- app/src/app/rating.service.ts
+++ app/src/app/rating.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IRating } from './model/rating.model';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  region: 'eu-central-1',
+  userPoolId: 'eu-central-1_GoAyFwgMI',
+  clientId: '3u6hqppau6cu5761q87a130k9j',
+  apiUrl:
+    'https://7uviw679il.execute-api.eu-central-1.amazonaws.com/SrbflixStage',
+};
