ef041c97ee14a1b9e53ec4b0a8d504ff3e0f9dff
Vladislav Radovic <vradovic522@gmail.com>
2024-07-04 15:42:48 +0200

    Change to sdk v3

--- cdk/lib/lambda/upload-metadata.ts
+++ cdk/lib/lambda/upload-metadata.ts
@@ -1,53 +1,62 @@
-import * as AWS from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  PutCommandInput,
+} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.TABLE_NAME || '';
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        const videoId = event.pathParameters?.movieId;
-        if (!videoId) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ message: 'Missing videoId in path parameters' }),
-            };
-        }
+const client = new DynamoDBClient({ region: 'eu-central-1' });
+const docClient = DynamoDBDocumentClient.from(client);
 
-        const requestBody = JSON.parse(event.body || '{}');
-        const { title, description, actors, directors, genres } = requestBody;
-
-        if (!title || !description || !actors || !directors || !genres) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ message: 'Missing required metadata fields' }),
-            };
-        }
-
-        const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
-            TableName: tableName,
-            Item: {
-                videoId: videoId,
-                title: title,
-                description: description,
-                actors: actors,
-                directors: directors,
-                genres: genres,
-                createdAt: new Date().toISOString(),
-            },
-        };
-
-        await dynamoDB.put(params).promise();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Video metadata stored successfully' }),
-        };
-    } catch (error) {
-        console.error('Error:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Internal server error' }),
-        };
+export const handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const videoId = event.pathParameters?.movieId;
+    if (!videoId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing videoId in path parameters' }),
+      };
     }
+
+    const requestBody = JSON.parse(event.body || '{}');
+    const { title, description, actors, directors, genres } = requestBody;
+
+    if (!title || !description || !actors || !directors || !genres) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing required metadata fields' }),
+      };
+    }
+
+    const params: PutCommandInput = {
+      TableName: tableName,
+      Item: {
+        videoId: videoId,
+        title: title,
+        description: description,
+        actors: actors,
+        directors: directors,
+        genres: genres,
+        createdAt: new Date().toISOString(),
+      },
+    };
+
+    await docClient.send(new PutCommand(params));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Video metadata stored successfully' }),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
 };
