a8caba3f9dfb6605fe48a537d32351edc5a0c93d
Vladislav Radovic <vradovic522@gmail.com>
2024-07-05 18:54:04 +0200

    Add sign out button

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RatingService } from './rating.service';
 import { CommonModule } from '@angular/common';
+import { CognitoService } from './cognito.service';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,22 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'app';
 
-  constructor(private ratingService: RatingService) {}
+  constructor(
+    private ratingService: RatingService,
+    private cognitoService: CognitoService,
+  ) {}
 
   testApi() {
     this.ratingService.getRatingForCurrentUser('1').subscribe((rating) => {
       console.log('Rating: ', rating);
     });
+  }
+
+  isLoggedIn() {
+    return this.cognitoService.getCurrentUser() !== null;
+  }
+
+  signOut() {
+    this.cognitoService.signOut();
   }
 }
--- app/src/app/cognito.service.ts
+++ app/src/app/cognito.service.ts
@@ -123,4 +123,8 @@ export class CognitoService {
       return null;
     }
   }
+
+  getCurrentUser() {
+    return this.userPool.getCurrentUser();
+  }
 }
