8c1342dfe5721af6c59698257cae81a6556554b1
Jelena Adamović <105116909+adamoviccj@users.noreply.github.com>
2024-07-08 03:21:16 +0200

    environment modification

--- app/src/app/create-content.service.ts
+++ app/src/app/create-content.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { IMetadata } from './model/metadata.model';
 import { Observable } from 'rxjs';
 
