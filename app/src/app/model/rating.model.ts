a3f2e428e24bd33cfa30b7fc0a9c629e7c066612
Vladislav Radovic <vradovic522@gmail.com>
2024-07-08 20:23:27 +0200

    Add rating feature

--- app/src/app/model/rating.model.ts
+++ app/src/app/model/rating.model.ts
@@ -1,5 +1,5 @@
 export interface IRating {
-  username: string;
-  movie_id: string;
+  username?: string;
+  movie_id?: string;
   rating: string;
 }
--- app/src/media-detail/media-detail.component.ts
+++ app/src/media-detail/media-detail.component.ts
@@ -3,29 +3,58 @@ import { Component, OnInit } from '@angular/core';
 import { IMetadata } from '../app/model/metadata.model';
 import { MediaService } from '../media.service';
 import { ActivatedRoute } from '@angular/router';
+import { RatingService } from '../app/rating.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-media-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './media-detail.component.html',
   styleUrl: './media-detail.component.scss',
 })
 export class MediaDetailComponent implements OnInit {
+  id: string = '';
   metadata?: IMetadata;
+  rating = 'none';
 
   constructor(
     private mediaService: MediaService,
+    private ratingService: RatingService,
     private route: ActivatedRoute,
   ) {}
 
   ngOnInit() {
-    const id: string = this.route.snapshot.params['id'];
-    console.log(id);
+    this.id = this.route.snapshot.params['id'];
+    console.log(this.id);
 
-    this.mediaService.getMetadata(id).subscribe((metadata) => {
+    this.mediaService.getMetadata(this.id).subscribe((metadata) => {
       this.metadata = metadata;
-      console.log(metadata);
     });
+
+    this.ratingService.getRating(this.id).subscribe((rating) => {
+      console.log(rating);
+      this.rating = rating.rating;
+    });
+  }
+
+  updateRating() {
+    if (!this.id) {
+      return;
+    }
+
+    if (this.rating === 'none') {
+      this.ratingService.deleteRating(this.id).subscribe(() => {
+        console.log('deleted');
+      });
+    }
+
+    this.ratingService
+      .createRating(this.id, {
+        rating: this.rating,
+      })
+      .subscribe((rating) => {
+        console.log(rating);
+      });
   }
 }
